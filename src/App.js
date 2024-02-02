cf2af544138626b3124d611f3c06b80d950bd6c3
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-02 23:48:27 +0530

    loading

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [dailyWeather, setDailyWeather] = useState(null);
   const [location, setLocation] = useState("");
   const [flag, setFlag] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const body = document.getElementById("body");
     if (body) {
